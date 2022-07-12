7c7ab5099e8d542fa3eba5c28774b36689d051b3
runarskagestad <runar.skagestad@nav.no>
2022-07-12 12:54:24 +0200

    mountPath for secret

--- secrets_handler.py
+++ secrets_handler.py
@@ -3,12 +3,12 @@ import json
 
 
 def create_envs_from_secret(secret_key: str):
-    #secrets_file = "/var/run/secrets/secrets"
+    secrets_file = "/var/run/secrets/ia-tjenester-metrikker-datafortelling-secrets/secret"
 
-    #with open(secrets_file) as file:
-    #    envs = json.loads(file.read())
+    with open(secrets_file) as file:
+        envs = json.loads(file.read())
 
-    envs = json.loads(os.environ["IA_METRIKKER_ENVS"])
+    # envs = json.loads(os.environ["IA_METRIKKER_ENVS"])
 
     for key, value in envs.items():
         if isinstance(value, dict):
