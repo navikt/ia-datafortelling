6c86f174fae844f0ae9037e3e647bb1424f2bf90
erlendaxelsson <erlend.axelsson@gmail.com>
2022-07-13 14:38:13 +0200

    Reenable plot

--- prepare_data.py
+++ prepare_data.py
@@ -7,9 +7,12 @@ def prep_data(data: pd.DataFrame) -> {}:
     temp["opprettet_year"] = temp["opprettet"].dt.year
     temp["opprettet_month"] = temp["opprettet"].dt.month
 
+    siste_12_måneder = temp.groupby(["opprettet_year", "opprettet_month"]).nunique().tail(12)
+    siste_12_måneder.index = siste_12_måneder.index.map(lambda multi_index: "{}/{}".format(multi_index[0], multi_index[1]))
+
     prepped = {
         "unike_bedrifter_per_år": temp.groupby("opprettet_year").nunique()["orgnr"],
-        "unike_bedrifter_per_måned": temp.groupby(["opprettet_year", "opprettet_month"]).nunique().tail(12)
+        "unike_bedrifter_per_måned": siste_12_måneder
     }
 
     return prepped
