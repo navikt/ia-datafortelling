ee1670d7ff90ddffe7628229a7a86d309c2056ea
Runar Skagestad <runska@gmail.com>
2023-10-11 10:47:26 +0200

    Add verbose logging to curl

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ quarto render leverte_ia_tjenester.qmd
 curl -X PUT \
      -F index.html=@index.html \
      https://${NADA_ENV}/quarto/update/${LEVERTE_IA_TJENESTER_QUARTO_ID} \
-     -H "Authorization: Bearer ${TEAM_TOKEN}"
+     -H "Authorization: Bearer ${TEAM_TOKEN}" -v
