95ac90e6fb5c2a8bec5dfc9f3c302a8f1d1eb20f
Runar Skagestad <runska@gmail.com>
2023-10-11 09:36:26 +0200

    Fikser kjøring med Naisjob

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 quarto render leverte_ia_tjenester.qmd
 
-curl -X PUT leverte_ia_tjenester.html \
-    https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
-    -H "Authorization:Bearer ${QUARTO_TOKEN}"
+curl -X PUT \
+     -F "index.html=@leverte_ia_tjenester.html" \
+     https://${NADA_ENV}/quarto/update/${LEVERTE_IA_TJENESTER_QUARTO_ID} \
+     -H "Authorization: Bearer ${TEAM_TOKEN}"
