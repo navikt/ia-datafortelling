22cae73386d9d7ad733d30f3afff991dcc4eea86
Runar Skagestad <runska@gmail.com>
2023-10-11 13:00:36 +0200

    read-only-file-system: "false"

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-quarto render leverte_ia_tjenester.qmd
+quarto render index.qmd
 
 curl -X PUT \
      -F index.html=@index.html \
      https://${NADA_ENV}/quarto/update/${LEVERTE_IA_TJENESTER_QUARTO_ID} \
-     -H "Authorization: Bearer ${TEAM_TOKEN}" -v
+     -H "Authorization: Bearer ${TEAM_TOKEN}"
