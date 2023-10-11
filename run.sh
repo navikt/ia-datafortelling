1a4b5ffbf63a47fa1d0b5ad7c224f6618138f32d
Runar Skagestad <runska@gmail.com>
2023-10-11 10:41:10 +0200

    Remove quites

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 quarto render leverte_ia_tjenester.qmd
 
 curl -X PUT \
-     -F "index.html=@index.html" \
+     -F index.html=@index.html \
      https://${NADA_ENV}/quarto/update/${LEVERTE_IA_TJENESTER_QUARTO_ID} \
      -H "Authorization: Bearer ${TEAM_TOKEN}"
