ec76fe9ff1040e327c5a45adbfb502ac9de5aa3b
Runar Skagestad <runska@gmail.com>
2024-01-25 13:51:17 +0100

    Fjerner key-metrics-datafortellingen

--- run.sh
+++ run.sh
@@ -8,10 +8,3 @@ quarto render leverte_ia_tjenester/index.qmd
 curl -X PUT -F index.html=@leverte_ia_tjenester/index.html \
      https://${NADA_ENV}/quarto/update/${LEVERTE_IA_TJENESTER_QUARTO_ID} \
      -H "Authorization: Bearer ${TEAM_TOKEN}"
-
-
-quarto render key_metrics/index.qmd
-
-curl -X PUT -F index.html=@key_metrics/index.html \
-    https://${NADA_ENV}/quarto/update/${KEY_METRICS_QUARTO_ID} \
-    -H "Authorization:Bearer ${TEAM_TOKEN}"
